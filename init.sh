8ce8738cf75f319b6a3f510af36936d4aa94f10d
Jevin G <thorncorona@gmail.com>
2017-08-09 21:49:02 -0700

    Add all other files

---
+++ init.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+export MONGO_URL="mongodb://localhost:27017/amberfish"
+export ROOT_URL="https://amberfish.storied.me"
+export MAIL_URL="smtp://dev@storied.me:storiedme@mail.storied.me:587" 
+
+ 
